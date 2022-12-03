94824facb9a85e94c88d26da280f8bdf42aa4251
Evan Walter <evan.walter.software@gmail.com>
2022-12-02 22:54:05 -0800

    wire routes to server.js

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,6 @@ const port = process.env.PORT || 5001
 
 const app = express()
 
-app.get('/api/goals', (req, res) => {
-  res.status(200).json({ message: 'Get goals' })
-})
+app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
