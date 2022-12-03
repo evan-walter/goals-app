ecd145b236c5d80b76b84ded280cb8f0771e5af2
Evan Walter <evan.walter.software@gmail.com>
2022-12-02 22:50:50 -0800

    start api

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('hello')
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const port = process.env.PORT || 5001
+
+const app = express()
+
+app.get('/api/goals', (req, res) => {
+  res.status(200).json({ message: 'Get goals' })
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
