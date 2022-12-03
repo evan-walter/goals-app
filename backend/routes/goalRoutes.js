0b2687813c4e3bcf3e9d17688b138bb9ed89f894
Evan Walter <evan.walter.software@gmail.com>
2022-12-03 12:28:37 -0800

    refactor: reduce code

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -7,12 +7,7 @@ const {
   deleteGoal,
 } = require('../controllers/goalController')
 
-router.get('/', getGoals)
-
-router.post('/', setGoal)
-
-router.put('/:id', updateGoal)
-
-router.delete('/:id', deleteGoal)
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').delete(deleteGoal).put(updateGoal)
 
 module.exports = router
