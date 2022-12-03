1e5a7d3c5d736ee7aa7e85c253f6f02d6c1aacab
Evan Walter <evan.walter.software@gmail.com>
2022-12-03 14:15:34 -0800

    add goal model to controller

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,14 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalModel')
+
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get goals' })
+  const goals = await Goal.find()
+
+  res.status(200).json(goals)
 })
 
 // @desc    Set goals
