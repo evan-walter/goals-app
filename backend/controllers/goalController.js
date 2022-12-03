12b4af858d544f851410da02dd4a281231dae247
Evan Walter <evan.walter.software@gmail.com>
2022-12-03 14:24:21 -0800

    feat: update goal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -31,7 +31,18 @@ const setGoal = asyncHandler(async (req, res) => {
 // @route   PUT /api/goals
 // @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` })
+  const goal = await Goal.findById(req.params.id)
+
+  if (!goal) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  })
+
+  res.status(200).json(updatedGoal)
 })
 
 // @desc    Delete goal
