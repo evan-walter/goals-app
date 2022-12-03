cf3d965950fc055d03eef1d761fc15bcd2170677
Evan Walter <evan.walter.software@gmail.com>
2022-12-03 14:31:15 -0800

    feat: delete goal, complete crud functionality

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -49,7 +49,16 @@ const updateGoal = asyncHandler(async (req, res) => {
 // @route   DELETE /api/goals
 // @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` })
+  const goal = await Goal.findById(req.params.id)
+
+  if (!goal) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+
+  await goal.remove()
+
+  res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
