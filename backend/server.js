d591a2a73ce4d49a423053452427a80f1bf4ceea
Evan Walter <evan.walter.software@gmail.com>
2022-12-03 12:51:48 -0800

    change express default error

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-
+const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5001
 
 const app = express()
@@ -9,5 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
