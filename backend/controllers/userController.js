b3f69f75619941894a65838d386cb5e30ad947b8
Evan Walter <evan.walter.software@gmail.com>
2022-12-07 20:58:06 -0800

    protect users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,9 +70,15 @@ const loginUser = asyncHandler(async (req, res) => {
 
 // @desc    Get user data
 // @route   GET /api/users/me
-// @access  Public
+// @access  Private
 const getMe = asyncHandler(async (req, res) => {
-  res.json({ message: 'User data display' })
+  const { _id, name, email } = await User.findById(req.user.id)
+
+  res.status(200).json({
+    id: _id,
+    name,
+    email,
+  })
 })
 
 // Generate AWT
