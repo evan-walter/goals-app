ef5789a54515bda33c5ab45eba6ee3874be162e9
Evan Walter <evan.walter.software@gmail.com>
2022-12-08 15:11:07 -0800

    wip: more frontend

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,6 +1,21 @@
+import { useState, useEffect } from 'react'
+
 function Register() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  })
+
+  const { name, email, password, password2 } = formData
+
   return (
-    <div>Register</div>
+    <>
+      <section className='heading'>
+        <h1></h1>
+      </section>
+    </>
   )
 }
 
