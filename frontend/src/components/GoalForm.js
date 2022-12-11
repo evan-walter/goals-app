66fa54a42614cefe0ab1fb47b97ab310a060bf80
Evan Walter <evan.walter.software@gmail.com>
2022-12-11 12:57:51 -0800

    scaffold redux goal functionality

---
+++ frontend/src/components/GoalForm.js
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+function GoalForm() {
+  return <div>GoalForm</div>
+}
+
+export default GoalForm
---
+++ frontend/src/features/goals/goalService.js
@@ -0,0 +1,4 @@
+import axios from 'axios'
+
+const API_URL = '/api/goals/'
+
