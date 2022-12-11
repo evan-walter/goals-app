2318d1399064f2301bc71cf5e0b9f7214fe1ea6c
Evan Walter <evan.walter.software@gmail.com>
2022-12-11 13:59:09 -0800

    wip: frontend goals functionality

--- frontend/src/components/GoalForm.js
+++ frontend/src/components/GoalForm.js
@@ -1,8 +1,40 @@
 import { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { createGoal } from '../features/goals/goalSlice'
 
 function GoalForm() {
-  return <div>GoalForm</div>
+  const [text, setText] = useState('')
+
+  const dispatch = useDispatch()
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    dispatch(createGoal(text))
+    setText('')
+  }
+
+  return (
+    <section className='form'>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label htmlFor='text'>Goal</label>
+          <input
+            type='text'
+            name='text'
+            id='text'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div className='form-group'>
+          <button className='btn btn-block' type='submit'>
+            Add Goal
+          </button>
+        </div>
+      </form>
+    </section>
+  )
 }
 
 export default GoalForm
--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -9,11 +9,46 @@ const initialState = {
   message: '',
 }
 
+// Create new goal
+export const createGoal = createAsyncThunk(
+  'goals/create',
+  async (goalData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token
+      return await goalService.createGoal(goalData, token)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+)
+
 export const goalSlice = createSlice({
   name: 'goal',
   initialState,
   reducers: {
     reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createGoal.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(createGoal.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.goals.push(action.payload)
+      })
+      .addCase(createGoal.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
   },
 })
 
