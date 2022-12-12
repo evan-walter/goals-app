36ac638a071e08b3a01b87b8823962e58f98a1d6
Evan Walter <evan.walter.software@gmail.com>
2022-12-11 19:40:38 -0800

    wip: delete goal

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import GoalForm from '../components/GoalForm'
+import GoalItem from '../components/GoalItem'
 import Spinner from '../components/Spinner'
 import { getGoals, reset } from '../features/goals/goalSlice'
 
@@ -40,7 +41,20 @@ function Dashboard() {
         <h1>Welcome {user && user.name}</h1>
         <p>Goals Dashboard</p>
       </section>
+
       <GoalForm />
+
+      <section className='content'>
+        {goals.length > 0 ? (
+          <div className='goals'>
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section>
     </>
   )
 }
